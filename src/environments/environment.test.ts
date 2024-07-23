040109588438198d21050401669ba1aece5e697f
SaidDig <said.chaibi@digidco.com>
2024-07-23 10:47:20 +0100

    Create environment.test.ts

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  test: true,
+  // url: 'http://102.110.0.33:8087/api/v1/agent',
+  url: 'http://192.168.1.165:8085/api/v1/agent',
+  urlNotification: 'http://102.110.0.33:8087/api/v1',
+  // url: 'http://102.110.0.33:8088/api/v1/agent' 
+  // url : 'https://2ded-196-229-6-112.ngrok-free.app/api/v1/agent'
+  // url: 'http://102.110.0.33:8086/api/v1/agent'
+
+
+};
